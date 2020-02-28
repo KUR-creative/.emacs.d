a9eed53ae541660c145b7ac597dbf12cf0572732
kur <rhdnfka94@gmail.com>
2020-02-28 22:25:50 +0900

    Feat: Add python mode f4 f5 command

---
+++ config/python3.el
@@ -0,0 +1,17 @@
+(require 'python)
+
+(defun run-main ()
+  "run main.py"
+  (interactive)
+  (save-some-buffers t)
+  (async-shell-command "python main.py"))
+
+
+(defun run-this ()
+  "run python file in current buffer"
+  (interactive)
+  (save-some-buffers t)
+  (async-shell-command (concat "python " (buffer-file-name))))
+
+(define-key python-mode-map (kbd "<f5>") 'run-main)
+(define-key python-mode-map (kbd "<f4>") 'run-this)

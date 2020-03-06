5652f2d3f4798471004c08bd0c2c5716fabf9229
kur <rhdnfka94@gmail.com>
2020-03-06 12:54:11 +0900

    Feat: set indent as 2 space in js

--- config/js.el
+++ config/js.el
@@ -1,7 +1,17 @@
+(defun set-js-indent=2 ()
+  (interactive)
+  (setq-local js-indent-level 2))
+(add-hook 'js-mode-hook 'set-js-indent=2)
+
+(defun set-js2-indent=2 ()
+  (interactive)
+  (setq-local js2-basic-offset 2))
+
 (use-package js2-mode
   :ensure t
   :config
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+  (add-hook 'js2-mode-hook 'set-js2-indent=2))
 
 (use-package rjsx-mode
   :ensure t

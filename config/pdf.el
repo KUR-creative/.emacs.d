012f37c1638b8cafbdcce70058e6626f5379b65d
kur <rhdnfka94@gmail.com>
2020-09-09 20:42:58 +0900

    Add windmove keybinding for pdf.el

--- config/pdf.el
+++ config/pdf.el
@@ -1,6 +1,10 @@
 (use-package doc-view
   :ensure t
   :config
-  (add-hook 'doc-view-mode-hook 'auto-revert-mode))
-  ;(define-key doc-view-mode-map "j" 'doc-view-next-page)
-  ;(define-key doc-view-mode-map "k" 'doc-view-previous-page)
+  (add-hook 'doc-view-mode-hook 'auto-revert-mode)
+  (define-key doc-view-mode-map (kbd "C-h") 'windmove-left)
+  (define-key doc-view-mode-map (kbd "C-j") 'windmove-down)
+  (define-key doc-view-mode-map (kbd "C-k") 'windmove-up)
+  (define-key doc-view-mode-map (kbd "C-l") 'windmove-right))
+ ;(define-key doc-view-mode-map "j" 'doc-view-next-page)
+ ;(define-key doc-view-mode-map "k" 'doc-view-previous-page)

cd7c3d9458aa669d25d877e8902d9a51afe13ab3
kur <rhdnfka94@gmail.com>
2021-04-19 16:24:06 +0900

    Edit display.el
    Add WARNING, THINK as hl-todo keyword
    Enable hl-todo in latex-mode, text-mode
    Toggle truncate lines
    Set font height as 105

--- config/display.el
+++ config/display.el
@@ -4,7 +4,7 @@
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
-;(toggle-truncate-lines) 
+;(toggle-truncate-lines)
 (set-default 'truncate-lines t)
 
 (global-display-line-numbers-mode) ;emacs 26
@@ -20,18 +20,23 @@
 ;111111111222222222333333333344444444445555555555666666666677777
 (set-face-attribute 'default nil
                     :family "Source Code Pro"
-                    :height 110
+                    :height 105
                     :weight 'normal
                     :width 'normal)
 
 (use-package hl-todo
-  :hook (prog-mode . hl-todo-mode)
+  :hook ((prog-mode . hl-todo-mode)
+         (latex-mode . hl-todo-mode)
+         (text-mode . hl-todo-mode)
+         )
   :config
   (setq hl-todo-highlight-punctuation ":"
         hl-todo-keyword-faces
         `(("TODO"       warning bold)
           ("FIXME"      error bold)
+          ("WARNING"    error bold)
           ("HACK"       font-lock-constant-face bold)
           ("REVIEW"     font-lock-keyword-face bold)
+          ("THINK"     font-lock-keyword-face bold)
           ("NOTE"       success bold)
           ("DEPRECATED" font-lock-doc-face bold))))
